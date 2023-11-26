39019f5691f7539ec74dbe11f7b92febd2a2b267
Harsh Kasat <hkasat@waymore.io>
2023-11-26 19:30:11 +0530

    Update code so It will make tweet short so it can come under twiiter tweet guidelines

--- Twitter_Api.py
+++ Twitter_Api.py
@@ -9,16 +9,17 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 load_dotenv()
+import time
 # Temporary module 
 
 class TwitterClient:
-    def __init__(self, tweet):
+    def __init__(self, *args):
         self.consumer_key = os.environ.get('API_KEY') 
         self.consumer_secret = os.environ.get('API_SECRET_KEY')
         self.access_token = os.environ.get('ACCESS_TOKEN')
         self.access_token_secret = os.environ.get('SECRET_ACCESS_TOKEN')
         self.bearer_token = os.environ.get('BEARER_TOKEN')
-        self.text = tweet
+        self.text = "".join(args)
     
     def post_tweet(self): 
         api = tweepy.Client(bearer_token=self.bearer_token,
@@ -57,9 +58,9 @@ class TwitterClient:
 class Template:
     """This class represents Creation of templates"""
 
-    def __init__(self, tool, tool_info):
-        self.tool = tool
-        self.tool_info = tool_info
+    def __init__(self, **kwargs):
+        self.tool = kwargs['tool']
+        self.tool_info = kwargs['tool_info']
 
     def get_template(self):
         try:
@@ -75,17 +76,23 @@ class LangchainPrompt:
 
     def __init__(self, template) :
         self.get_template = template
+        self.wait_time = 5
     
     def LLMPromptTemplate(self):
+        
         try:
-            # print(self.template)
-            # prompt = PromptTemplate.from_template(template=self.get_template)
+    
             llm = OpenAI(openai_api_key = os.environ.get('OPENAI_API_KEY'))
             tweet = llm.predict(self.get_template)
-                
-            return (tweet)
+            if len(tweet) <= 280:
+                return tweet    
+            else:
+                time.sleep(self.wait_time)
+                self.LLMPromptTemplate()
+            return tweet
         except Exception as e:
-            print(e)
+            print(f"Rate limit exceeded. Retrying in {self.wait_time} seconds. Error: {e}")
+            
 
 class ToolSpecficTask:
     """Random Tool and Infomation about Tool from the csv file."""
@@ -99,7 +106,12 @@ class ToolSpecficTask:
             df = pd.read_csv('tools-specific_task.csv')
             index = self.random_word()
             tool, tool_info = df.iloc[index,[0,1]]
-            return (tool, tool_info)
+
+            tools = {
+                "tool": tool,
+                "tool_info": tool_info,
+            }
+            return tools
         except Exception as e:
             print(e)
 
@@ -143,10 +155,10 @@ def main():
 
     # Get Random Tool and Information about Tool
     get_tool = ToolSpecficTask()
-    get_tool, get_tool_info = get_tool.get_tool()
+    get_tool  = get_tool.get_tool()
     
     # Get Template about Tool and Information about Tool for GPT model or Custom model
-    get_template = Template(tool=get_tool, tool_info=get_tool_info) 
+    get_template = Template(**get_tool) 
     get_template = get_template.get_template()
 
     # Generate Tweet about Tool and Information about Tool using Template
@@ -154,14 +166,14 @@ def main():
     get_tweet = get_tweet.LLMPromptTemplate()
 
     # Make Api cal To X(Twitter) to Tweet
-    post_tweet = TwitterClient(tweet=get_tweet)
+    post_tweet = TwitterClient(*get_tweet)
     post_tweet = post_tweet.post_tweet()
 
-    # save the tweet in Dataframe using pandas
-    save_tweet = SaveTweet(tool=get_tool, tool_info=get_tool_info, tweet=get_tweet)
+    # # save the tweet in Dataframe using pandas
+    save_tweet = SaveTweet(tool=get_tool['tool'], tool_info=get_tool['tool_info'], tweet=get_tweet)
     save_tweet = save_tweet.savetweet()
 
-    # post_tweet = True
+    # # post_tweet = True
     if post_tweet and save_tweet:
         print("Tweet Successfully")
     else:
